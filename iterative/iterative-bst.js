a53d0b367e808e17b769a1771f9444db7873da6a
Jackie Watanabe <sushiigirl@gmail.com>
2017-10-08 22:54:54 -0700

    added iterative implementations of bst search and insert

---
+++ iterative/iterative-bst.js
@@ -0,0 +1,57 @@
+function Node(value) {
+  this.data = value;
+  this.left = null;
+  this.right = null;
+}
+
+function Tree() {
+  this.root = null;
+}
+
+Tree.prototype.insert = function(value) {
+  node = new Node(value);
+  if (this.root === null) {
+    this.root = node;
+    return node;
+  }
+  var current = this.root;
+
+  while (current != null) {
+    if (value <= current.data && !current.left) {
+      return current.left = node;
+    } else if (value > current.data && !current.right) {
+      return current.right = node;
+    } else if (value <= current.data && current.left) {
+      current = current.left;
+    } else if (value > current.data && current.right) {
+      current = current.right;
+    }
+  }
+};
+
+Tree.prototype.search = function(value) {
+  var current = this.root;
+  if (current === null) {
+    return false;
+  }
+
+  while (current != null) {
+    if (value === current.data) {
+      return true;
+    } else if (value < current.data) {
+      current = current.left;
+    } else if (value > current.data) {
+      current = current.right;
+    }
+  }
+  return false;
+}
+
+var myTree = new Tree();
+
+console.log(myTree.insert(8));
+console.log(myTree.insert(3));
+console.log(myTree.insert(10));
+console.log(myTree.insert(6));
+console.log(myTree.search(6));
+console.log(myTree.root);
